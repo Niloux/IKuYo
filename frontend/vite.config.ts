a46cde1c523b39ee149477cf0065b087037fdf61
Niloux3d <15072763003@163.com>
2025-07-07 11:30:37 +0800

    feat: 在vite.config.ts中新增gzip和brotli压缩插件，优化构建性能；调整构建配置以实现手动分块，提高代码管理效率

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,16 +3,47 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    viteCompression({
+      algorithm: 'gzip',
+      threshold: 10240, // 10KB
+      ext: '.gz',
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      threshold: 10240, // 10KB
+      ext: '.br',
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) {
+              return 'vendor_vue';
+            }
+            if (id.includes('pinia')) {
+              return 'vendor_pinia';
+            }
+            if (id.includes('vue-router')) {
+              return 'vendor_router';
+            }
+            return 'vendor';
+          }
+        },
+      },
     },
   },
 })
