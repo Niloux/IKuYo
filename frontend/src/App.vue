e4ec6bf7a337c665f436f870c2f78a44751f1713
Niloux3d <15072763003@163.com>
2025-07-07 14:02:52 +0800

    feat: 简化App.vue中的路由视图逻辑，优化组件渲染条件，提升页面切换性能

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,18 +7,14 @@ const router = useRouter()
 
 <template>
   <AppLayout>
-    <RouterView v-slot="{ Component, route }">
-      <template v-if="route && Component">
-        <keep-alive v-if="route.meta && route.meta.keepAlive">
-          <transition name="fade-page" mode="out-in">
-            <component :is="Component" :key="route.fullPath" />
-          </transition>
-        </keep-alive>
-        <transition v-else name="fade-page" mode="out-in">
+    <transition name="fade-page" mode="out-in">
+      <RouterView v-slot="{ Component, route }">
+        <keep-alive v-if="route.meta.keepAlive">
           <component :is="Component" :key="route.fullPath" />
-        </transition>
-      </template>
-    </RouterView>
+        </keep-alive>
+        <component :is="Component" v-else :key="route.fullPath" />
+      </RouterView>
+    </transition>
   </AppLayout>
 </template>
 
